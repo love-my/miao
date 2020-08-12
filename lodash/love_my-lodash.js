9d2a7f7de5bf9bdc5c8206e497a1e92405a6fce4
my <my@qq.com>
2020-08-12 19:24:33 +0800

    lodash.js

--- lodash/love_my-lodash.js
+++ lodash/love_my-lodash.js
@@ -2401,7 +2401,7 @@ var love_my = {
         } else {
           var b = obj[key]
           var c =a[key]
-          if (typeof(b) == typeof(c) && typeof(b) == 'object' && !Array.isArray(b)) {
+          if (typeof(b) == 'object' && (Array.isArray(b) && Array.isArray(c)) || (!Array.isArray(b) && !Array.isArray(c))) {
             this.merge(b, c)
           } else {
             b = c
